5badf92024d9d72e6141157b770752510697a5e8
Gabiel Alexander <gaboquintana10@gmail.com>
2025-04-27 14:43:35 -0600

    Arregle todos los erroes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import InicioSesion from './components/InicioSesion';
 import RecuperacionContra from './components/RecuperacionContra';
 import PaginaDetalleProducto from './components/PaginaDetalleProducto';
 import PaginaInicio from './components/PaginaInicio'; // Importamos el nuevo componente
-import PaginaProductos from './components/ProductPage';
+import PaginaProductos from './components/PaginaProductos';
 function App() {
   const [count, setCount] = useState(0);
 
